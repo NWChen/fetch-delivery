ce7b132a698a8e3e4da6ce8e91f064ec7531c61e
MargaretQian <myq2000@columbia.edu>
2017-04-19 17:53:59 -0400

    connect to db

--- server_template.py
+++ server_template.py
@@ -14,23 +14,23 @@ from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
 ### Neil's imports
-import csv
-import copy
-import actionlib
-import rospy
-import roslib
+# import csv
+# import copy
+# import actionlib
+# import rospy
+# import roslib
 
-import threading
+# import threading
 
-from math import sin, cos
-from moveit_python import (MoveGroupInterface,
-                           PlanningSceneInterface,
-                           PickPlaceInterface)
-from moveit_python.geometry import rotate_pose_msg_by_euler_angles
+# from math import sin, cos
+# from moveit_python import (MoveGroupInterface,
+#                            PlanningSceneInterface,
+#                            PickPlaceInterface)
+# from moveit_python.geometry import rotate_pose_msg_by_euler_angles
 
-from geometry_msgs.msg import PoseStamped
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
+# from geometry_msgs.msg import PoseStamped
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+# from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -59,7 +59,16 @@ def teardown_request(exception):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = g.conn.execute("SELECT L.name FROM locations L")
+    data = []
+    for result in cursor:
+      entry_dict = {"location": result[0]}
+      data.append(entry_dict)
+    cursor.close()
+
+    context = dict(data = data)
+
+    return render_template('index.html', **context)
 
 @app.route('/pos/', methods=['POST'])
 def pos():
@@ -71,26 +80,26 @@ def pos():
     return render_template('index.html')
 
 
-# Move base using navigation stack
-class MoveBaseClient(object):
+# # Move base using navigation stack
+# class MoveBaseClient(object):
 
-    def __init__(self):
-        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        rospy.loginfo("Waiting for move_base...")
-        self.client.wait_for_server()
+#     def __init__(self):
+#         self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+#         rospy.loginfo("Waiting for move_base...")
+#         self.client.wait_for_server()
 
-    def goto(self, x, y, theta, frame="map"):
-        move_goal = MoveBaseGoal()
-        move_goal.target_pose.pose.position.x = x
-        move_goal.target_pose.pose.position.y = y
-        move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
-        move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
-        move_goal.target_pose.header.frame_id = frame
-        move_goal.target_pose.header.stamp = rospy.Time.now()
+#     def goto(self, x, y, theta, frame="map"):
+#         move_goal = MoveBaseGoal()
+#         move_goal.target_pose.pose.position.x = x
+#         move_goal.target_pose.pose.position.y = y
+#         move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
+#         move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
+#         move_goal.target_pose.header.frame_id = frame
+#         move_goal.target_pose.header.stamp = rospy.Time.now()
 
-        # TODO wait for things to work
-        self.client.send_goal(move_goal)
-        self.client.wait_for_result()
+#         # TODO wait for things to work
+#         self.client.send_goal(move_goal)
+#         self.client.wait_for_result()
 
 
 if __name__ == "__main__":
@@ -107,26 +116,26 @@ if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
-  # start webserver on a separate thread from ros
-  t = threading.Thread(target=run)
-  t.daemon=True
-  t.start()
+  # # start webserver on a separate thread from ros
+  # t = threading.Thread(target=run)
+  # t.daemon=True
+  # t.start()
 
-  # Make sure sim time is working
-  while not rospy.Time.now():
-      pass
+  # # Make sure sim time is working
+  # while not rospy.Time.now():
+  #     pass
 
-  # Setup clients
-  move_base = MoveBaseClient()
+  # # Setup clients
+  # move_base = MoveBaseClient()
 
-  while not rospy.is_shutdown():
+  # while not rospy.is_shutdown():
 
-      # prevent assumption that destination is (0, 0)
-      if x == 0 and y == 0:
-          continue
-      rospy.loginfo(x)
-      rospy.loginfo(y)
-      move_base.goto(x, y, 0.0)
+  #     # prevent assumption that destination is (0, 0)
+  #     if x == 0 and y == 0:
+  #         continue
+  #     rospy.loginfo(x)
+  #     rospy.loginfo(y)
+  #     move_base.goto(x, y, 0.0)
 
 
   run()
